27bb08a9aa928cd54e8f57cdb2db1e27d9a324a4
Aleks <aleks.jevtic315@gmail.com>
2021-03-05 19:41:16 +0100

    Algos: Inital setup übung 1 yahoo finance

--- SEM2_SS2020/Algorithmen_Datenstrukturen/Übung_1/Übung_1/main.cpp
+++ SEM2_SS2020/Algorithmen_Datenstrukturen/Übung_1/Übung_1/main.cpp
@@ -2,31 +2,7 @@
 #include <list>
 using namespace std;
 
-class HashTable {
-private:
-	string name;
-	struct student
-	{
-		string name;
-		int martikelnummer;
-	};
-
-	list<student> list_objects;
-	const int prime_number = 101;
-};
-
-int hash_function(string input_string)
-{
-	int n = input_string.length();
-	int hash =0;
-	for (int i = 0; i < n; i++)
-	{
-		return;
-	}
-}
-
 int main()
 {
-	string my_input;
-
+	cout << "Hello World!";
 }
