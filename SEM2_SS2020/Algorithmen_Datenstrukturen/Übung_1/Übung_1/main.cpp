fdc1f6385c66c7c3f5786c7a2016bb0db17c9bf5
Aleks <aleks.jevtic315@gmail.com>
2021-03-06 22:56:57 +0100

    Algorithmen & Datenstrukturen BSP 1.9 a-blatt 1

--- SEM2_SS2020/Algorithmen_Datenstrukturen/Übung_1/Übung_1/main.cpp
+++ SEM2_SS2020/Algorithmen_Datenstrukturen/Übung_1/Übung_1/main.cpp
@@ -2,7 +2,53 @@
 #include <list>
 using namespace std;
 
+
+//Übungsblatt Bsp 1.9
+
+int pow_function(int number,int power)
+{
+	long long unsigned int sum = 1;
+	for(int i = power; i!=0;i--)
+	{
+		sum=(sum*number) % 99991;
+
+	}
+	return sum;
+}
+
+int hash_function(char character, int iteration, int length)
+{ long long unsigned int y = ((int)character * pow_function(31,(length-iteration-1)))%99991;
+	
+	return y;
+
+}
+
 int main()
 {
-	cout << "Hello World!";
+
+	char string[] ="FHTechnikumWien";
+	int length = 0;
+	for(int i = 0; ;i++)
+	{
+		if(string[i]=='\0')
+		break;
+		length++;
+	}
+	cout << length << endl;
+	int index = 0;
+	char symbol;
+	long long unsigned int x = 0;
+	long long unsigned int y = 0;
+	while(42)	{
+		symbol = string[index];
+		if(symbol == '\0')
+		break;
+		y = hash_function(symbol,index,length);
+		x +=y;
+		
+		cout << symbol <<" -> " << y << endl;
+		index++;
+	}
+	cout << string << "->" << x << endl;
+	cout << string << "->" << x%99991 << endl;
 }
